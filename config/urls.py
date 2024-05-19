7defcb9c595cc49e75bd348592cac6e785a18ce6
Aibek-Kozonberdiev <ratroniii@gmail.com>
2024-05-20 01:33:41 +0600

    second commit

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('apps.rickrolls.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT,}),]
